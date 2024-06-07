e54f737219300e842848729000341e6b5ddc3777
austinbowers <austin.bowers75@gmail.com>
2024-06-06 23:11:37 -0500

    Test deploy.

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -4,7 +4,7 @@
             <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Me</h2>
             <p class="mb-8 lg:mb-10 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">If you'd like to get in touch, please fill out the form below.</p>
             <div className="formcarry-container">
-                <form @submit.prevent="onSubmit" class="space-y-8">
+                <form @submit.prevent="onSubmit" class="space-y-8 ">
                     <div className="formcarry-block">
                         <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Name</label>
                         <input type="text" v-model="name" id="name" placeholder="John Doe" class="shadow-sm dark:bg-opacity-50 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg outline-none focus:ring-emerald-500 focus:border-emerald-500 block w-full p-2.5 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-emerald-500 dark:focus:border-emerald-500 dark:shadow-sm-light"/>
